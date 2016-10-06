62053300c5c5f74544155088fcd4146594ebda49
Mnenmenth <iamrazim12@gmail.com>
2016-10-06 12:59:04 -0400

    Init MainFrame and ContentPane

--- src/main/scala/core/Main.scala
+++ src/main/scala/core/Main.scala
@@ -1,11 +1,21 @@
 package core
 
+import java.awt.Dimension
+
+import scala.swing.{MainFrame, SimpleSwingApplication}
+
 /**
   * Created by Mnenmenth Alkaborin
   * Please refer to LICENSE file if included
   * for licensing information
   * https://github.com/Mnenmenth
   */
-object Main {
+object Main extends SimpleSwingApplication {
+
+  val windowSize = new Dimension(800, 600)
+
+  def top = new MainFrame {
+    size = windowSize
+  }
 
 }
